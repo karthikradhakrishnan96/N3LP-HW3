925a413f397295c3bc31b9e861f71520bc8233f2
Karthik Radhakrishnan <karthikradhakrishnan96@gmail.com>
2020-05-05 03:11:13 -0400

    Added feats + extra layer + dropout

--- taskA/wrappers/HfModelWrapper.py
+++ taskA/wrappers/HfModelWrapper.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-
+import torch.nn.init as init
 from constants.constants import *
 from transformers import BertModel
 import torch.nn.functional as F
@@ -13,8 +13,14 @@ class HfModelWrapper(nn.Module):
         self.bert = BertModel.from_pretrained(bert_name)
         self.feats_len = 34
         self.mid = nn.Linear(2 * EMBED_SIZE + self.feats_len, 512)
-        self.out = nn.Linear(512, NUM_CLASSES)
+        self.mid2 = nn.Linear(512, 64)
+        self.out = nn.Linear(64, NUM_CLASSES)
+        self.dropout2 = nn.Dropout(0.17)
         self.dropout = nn.Dropout(HIDDEN_DROPOUT)
+        init.kaiming_normal_(self.mid.weight, mode='fan_in')
+
+        init.kaiming_normal_(self.out.weight, mode='fan_in')
+        init.kaiming_normal_(self.mid2.weight, mode='fan_in')
 
     def forward(self, input_ids, token_ids, mask_ids=None, input_ids2=None, token_ids2=None, mask_ids2=None,
                 feats=None):
@@ -25,6 +31,7 @@ class HfModelWrapper(nn.Module):
         if feats is not None:
             bert_outputs = torch.cat([bert_outputs, feats], dim=1)
         bert_outputs = self.dropout(bert_outputs)
-        bert_outputs = F.relu(self.mid(bert_outputs))
+        bert_outputs = self.dropout2(F.relu(self.mid(bert_outputs)))
+        bert_outputs = F.relu(self.mid2(bert_outputs))
         logits = self.out(bert_outputs)
         return logits
